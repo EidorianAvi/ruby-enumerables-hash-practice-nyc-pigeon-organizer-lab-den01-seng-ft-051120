05bbf111c587df69b3c17cacb713c56b8468a0d6
Rene Avila <reneavila1993@gmail.com>
2020-05-08 18:39:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,35 @@
+# def nyc_pigeon_organizer(data)
+#   pigeon_list = {}
+#   data.each do |key, value| #key = c,g,l value = {}
+#     value.each do |descriptor, array|
+#       array.each do |name|
+#         if pigeon_list[name] == nil 
+#           pigeon_list[name] = {}
+#         end
+#         if pigeon_list[name][key] == nil
+#           pigeon_list[name][key] = []
+#         end
+#         pigeon_list[name][key] << descriptor.to_s 
+#       end 
+#     end 
+#   end
+#   pigeon_list
+# end 
+
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
-  data.each do |key, value| #key = c,g,l value = {}
-    value.each do |descriptor, array|
-      array.each do |name|
-        if pigeon_list[name] == nil 
-          pigeon_list[name] = {}
-        end
-        if pigeon_list[name][key] == nil 
-          pigeon_list[name][key] = []
-        end
-        pigeon_list[name][key] << descriptor.to_s 
-      end
-    end
-  end
+  pigeon_list = Hash.new
+   data.each do |cgl, value|
+     value.each do |quality, array|
+       array.each do |name|
+         if pigeon_list[name] == nil
+           pigeon_list[name] = {}
+         end
+         if pigeon_list[name][value] == nil
+           pigeon_list[name][] = []
+         end
+         pigeon_list[name][key] << quality.to_s
+       end
+     end
+   end
   pigeon_list
 end
